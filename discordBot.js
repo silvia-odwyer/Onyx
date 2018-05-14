bc81db04aca59edff2af938ee726ac6c307c27c3
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-05-14 12:31:14 +0100

    Bot can now get the meanings of acronyms and abbreviations, thanks to a cool database which houses all acronym meanings.

--- discordBot.js
+++ discordBot.js
@@ -201,6 +201,164 @@ client.on('message', msg => {
         }
 
     }
+
+    else if (cmd === "acronym"){
+        var acronym = msg_array[1];
+        console.log(msg_array)
+        msg.reply("Pinging Acronym Database for some cool acronym meanings . . .")
+        var acronym_uri = `http://acronyms.silmaril.ie/cgi-bin/xaa?${acronym}`;
+
+        request(acronym_uri, { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            //console.log(body);
+            var split_body = body.split("\n");
+            //console.log(split_body)
+            var num_acronyms = split_body[4];
+            if (num_acronyms.includes("0")){
+                msg.reply("No acronyms exist with this abbreviation.")
+            }
+            else{
+                var header = "```ml" + "\n" +
+                "Acronym Meanings for " + acronym + "👀 \n" +
+                "```"
+                msg.channel.send(header)
+                for (var i = 6; i < split_body.length - 1; i += 4){
+                    var line = split_body[i]
+                    line = line.trim()
+
+                    var split_acr_array = line.split(" ");
+
+                    console.log(split_acr_array)
+                    var first_item = split_acr_array[0]
+                    console.log("First item" + first_item)
+                    if (split_acr_array.length === 1){
+                        first_item = first_item.slice(7, first_item.length - 8)
+                        split_acr_array[0] = first_item
+
+                    }
+                    else{    
+
+                        var strpd_item = first_item.slice(7, first_item.length + 5);
+                        split_acr_array[0] = strpd_item;
+    
+                        
+                        var last_item = split_acr_array[split_acr_array.length - 1];
+                        var strpd_last_item = last_item.slice(0, split_acr_array.length - 11);
+                        split_acr_array[split_acr_array.length - 1] = strpd_last_item;
+                    }
+                
+                    console.log(split_acr_array)
+                    var final_acronym = split_acr_array.toString()
+                    final_acronym = final_acronym.split(",").join(" ")
+                    msg.channel.send(final_acronym)
+
+            }
+        }
+        });
+        
+    }
+
+
+    // if (cmd === "wiki"){
+    //     var search_term = msg_content.slice(5, msg_content.length);
+    //     console.log(search_term)
+    //     search_term = search_term.split(" ").join("%20");
+    //     console.log(search_term)
+    //     var wiki_link = `https://en.wikitionary.org/w/api.php?action=query&titles=${search_term}&prop=revisions&rvprop=content&format=json&formatversion=2`
+    //     fetch(wiki_link)
+    //     .then(res => res.json())
+    //     .then((out) => {
+    //     var wiki_info = out;
+    //     var wiki_content = wiki_info.query.pages[0].revisions[0].content;
+    //     console.log(wiki_content)
+    //     msg.reply(wiki_content)
+    //     })
+    //     .catch(err => { throw err });
+        
+    // }
+    
+    // HELP COMMAND
+    else if (cmd === 'help') {
+        if (msg_array.length > 1 ){
+           
+            var cmd = msg_array[1];
+            var cmd_example = cmd + "_examples";
+            var cmd_info = cmd_info_obj.commands[0][cmd];
+            console.log("Definition: " + cmd_info)
+            msg.channel.send("`" + cmd + "`" + " " + cmd_info)
+
+            var examples = cmd_info_obj.examples[0][cmd_example];
+            
+            if (examples != ""){
+                console.log(examples)
+            var examples_template = "```ml" + "\n" +
+            "Example Commands 👀" + "\n" +
+            "\n" +
+            "```" + "`" + examples + "`"
+            msg.channel.send(examples_template)
+            }
+        }
+        else {
+            var help_output = ""
+            var search_cmds = "`news` `population` `translate` `search` `define` `bitcoin` `acronym` "
+            var space_cmds = "`neo` `earth` `iss` `astronauts` "
+            var fun_cmds = "`xkcd` `qr` `!meme` `identify` `fmt`"
+
+            var search_header = "```ml" + "\n" +
+            "Info Commands 🔍" + "\n" +
+            "```"
+            help_output += search_header
+            help_output += search_cmds
+
+
+            var space_header = "```ml" + "\n" +
+            "Space Commands 🌌🌠🌃" + "\n" +
+            "```"
+            help_output += space_header
+            help_output += space_cmds
+
+            var fun_header = "```ml" + "\n" +
+            "Fun Commands 🎲 ✨🌈" + "\n" +
+            "```"
+            help_output += fun_header
+            help_output += fun_cmds
+            var help_message = "```ml" + "\n" +
+            "To get more info. on a certain command, plus examples, type `help command` where command is one of those above." + "\n" +
+            "```" + "eg: `help qr`"
+            help_output += help_message
+            
+            msg.channel.send(help_output)
+            console.log(help_output)
+            var examples = "```ml" + "\n" +
+            "Example Commands 👀" + "\n" +
+            "define dancing" + "\n" +
+            "```"
+        
+        }
+
+    
+        var general_cmds = "```glsl" + "\n" +
+        "avatar : POWER UP that avatar of yours. " + "\n" +
+        "```"
+    
+        var general_cmds2 = "```tex" + "\n" +
+        "avatar : POWER UP that avatar of yours. " + "\n" +
+        "```"
+    
+
+        var avatar_cmd = "```tex" + "\n" +
+        "avatar : $ POWER UP that avatar of yours." + "\n" + 
+        "```"
+        
+        var help = "\n `pls react` : Get a tonload of reactions for no reason. 👀 🤣 😃 😄 😅 😆 😉" +
+        "\n `define` : Get an old-style definition of a word. Try it, it's hilarious XD  \n `earth` : Get a live photo of the Earth as taken from NOAA DSCVRY."
+    
+        var reddit = "\n ```ml " + "\n" +
+        "Reddit Commands" + "\n" +
+        "```"
+   
+      }
+
     
    
 
