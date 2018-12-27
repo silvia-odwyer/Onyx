a056545a6cfaf2309866fdcbc8502ba9c20838cf
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2018-12-27 15:31:48 +0000

    GIF sending now enabled for wave cmd.

--- commands/social/wave.js
+++ commands/social/wave.js
@@ -3,6 +3,9 @@ const oneLine = require('common-tags').oneLine;
 const fetch = require('node-fetch');
 const request = require("request");
 
+var giphy_creds_obj = require("giphy_creds.json");
+var giphy_api_key = giphy_creds_obj["api_key"];
+
 var name = "wave"
 module.exports = class WaveCommand extends commando.Command {
     constructor(client) {
@@ -34,7 +37,33 @@ module.exports = class WaveCommand extends commando.Command {
         var sender = msg.message.author;
         var receiver = text.split(" ")[0];
 
-        msg.channel.send(`${sender} just waved at ${receiver} :wave:`)
+        msg.channel.send(`${sender} just waved at ${receiver} :wave:`);
+
+        let limit = 5;
+        let search_terms = ["wave", "hello", "hi", "greeting"];
+        let randomNumber = getRandomNumber(0, search_terms.length - 1);
+        let search_term = search_terms[randomNumber];
+
+        var giphy_endpoint = `https://api.giphy.com/v1/gifs/translate?rating=g&api_key=${giphy_api_key}&limit=${limit}&s=${search_term}`
+
+        fetch(giphy_endpoint)
+        .then(res => res.json())
+        .then((out) => {
+            console.log(out)
+            var giphy_link = out.data.bitly_gif_url
+            console.log(giphy_link)
+            msg.channel.send(giphy_link)
+
+            // Send an embed with a local image inside
+            msg.channel.send({
+                files: [
+                    "media/powered_by_giphy.png",
+                ]
+            })
+                .catch(console.error);
+
+        })
+        .catch(err => { throw err });
 
         function getRandomNumber(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
